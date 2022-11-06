2ae4517ba5fe13b8fe23e43e6eea93e21148fd48
lukecarr <lukecarr@users.noreply.github.com>
2022-11-06 21:27:54 +0000

    deploy: 9a2b55f487695462199fe7edff4e64f09beaafea

---
+++ assets/guide_async.md.d9252288.lean.js
@@ -0,0 +1,1 @@
+import{_ as s,c as a,o as n,a as o}from"./app.182ff1de.js";const A=JSON.parse('{"title":"Using asynchronously","description":"","frontmatter":{},"headers":[],"relativePath":"guide/async.md","lastUpdated":1667769675000}'),l={name:"guide/async.md"},p=o("",4),e=[p];function t(c,r,y,D,i,C){return n(),a("div",null,e)}const u=s(l,[["render",t]]);export{A as __pageData,u as default};
